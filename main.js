38ce571f6590cfa58140f70efb03d34a0cbb3f20
Hemil78 <zanattahemil@gmail.com>
2021-09-16 15:31:40 +0200

    vue-hello

---
+++ main.js
@@ -0,0 +1,18 @@
+
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const app = new Vue({
+        el: '#root',
+        data: {
+        happyDay : "Good Morning World !",
+        logo: 'https://fr.cdn.v5.futura-sciences.com/buildsv6/images/wide1920/6/8/c/68ce9326b3_122220_terre-espace-vitesse-02.jpg'
+ 
+        }
+    }
+);
+
+
+
