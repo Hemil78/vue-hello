016660e006d6a20d6dd5e37ff57144ba34ad33fa
Hemil78 <zanattahemil@gmail.com>
2021-09-17 11:21:04 +0200

    correzione e evento click

--- main.js
+++ main.js
@@ -10,9 +10,16 @@ const app = new Vue({
         happyDay : "Good Morning World !",
         logo: 'https://fr.cdn.v5.futura-sciences.com/buildsv6/images/wide1920/6/8/c/68ce9326b3_122220_terre-espace-vitesse-02.jpg'
  
+        },
+        methods: {
+                cambioSlide() {
+                        this.logo = "https://carraro1927.com/site/wp-content/themes/carraro/img/home/pro-banner-w-desktop.jpg",
+                        this.happyDay = "Are you Ready ?"
+                }
         }
-    }
-);
+});
+             
+                        
 
 
 
